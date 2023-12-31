6be538df2e204897dcb260da903f67da7560aa6a
y-l-mochizuki <mczk9402@gmail.com>
2023-12-31 18:41:32 +0900

    get tasks api

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,7 +1,21 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.orm import Session
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+
+def get_tasks_with_done(db: Session) -> list[tuple[int, str, bool]]:
+    result: Result = db.execute(
+        select(
+            task_model.Task.id,
+            task_model.Task.title,
+            task_model.Done.id.isnot(None).label("done"),
+        ).outerjoin(task_model.Done)
+    )
+
+    return result.all()
 
 
 def create_task(db: Session, task_create: task_schema.TaskCreate) -> task_model.Task:
