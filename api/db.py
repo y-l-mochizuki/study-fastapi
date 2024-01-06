57ce526d11cd34c04e4d58d73956142e5cc09da2
y-l-mochizuki <mczk9402@gmail.com>
2024-01-06 14:33:01 +0900

    fix: db

--- api/db.py
+++ api/db.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
-
-db_engine = create_engine(DB_URL, echo=True)
-db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+ASYNC_DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
 
 Base = declarative_base()
 
 
-def get_db():
-    with db_session() as session:
+async def get_db():
+    async with async_session() as session:
         yield session
