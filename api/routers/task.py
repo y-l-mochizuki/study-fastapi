168f9847e8a5fa8513ee255a4b4cc2961eb34817
y-l-mochizuki <mczk9402@gmail.com>
2024-01-06 15:16:40 +0900

    fix: put

--- api/routers/task.py
+++ api/routers/task.py
@@ -27,10 +27,10 @@ async def create_task(
 async def update_task(
     task_id: int, task_body: task_schema.TaskCreate, db: Session = Depends(get_db)
 ):
-    task = task_crud.get_task(db, task_id=task_id)
+    task = await task_crud.get_task(db, task_id=task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
-    return task_crud.update_task(db, task_body, original=task)
+    return await task_crud.update_task(db, task_body, original=task)
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
