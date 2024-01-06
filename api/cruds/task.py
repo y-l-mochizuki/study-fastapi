f18c183fd2b37967543aa6a4c979d1d511aca980
y-l-mochizuki <mczk9402@gmail.com>
2024-01-06 15:11:40 +0900

    fix: get_task & update_task & create_task

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,13 +1,14 @@
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
 
 
-def get_tasks_with_done(db: Session) -> list[tuple[int, str, bool]]:
-    result: Result = db.execute(
+async def get_tasks_with_done(db: AsyncSession) -> list[tuple[int, str, bool]]:
+    result: Result = await db.execute(
         select(
             task_model.Task.id,
             task_model.Task.title,
@@ -18,29 +19,31 @@ def get_tasks_with_done(db: Session) -> list[tuple[int, str, bool]]:
     return result.all()
 
 
-def get_task(db: Session, task_id: int) -> task_model.Task | None:
-    result: Result = db.execute(
+async def get_task(db: AsyncSession, task_id: int) -> task_model.Task | None:
+    result: Result = await db.execute(
         select(task_model.Task).filter(task_model.Task.id == task_id)
     )
 
     return result.scalars().first()
 
 
-def update_task(
+async def update_task(
     db: Session, task_create: task_schema.TaskCreate, original: task_model.Task
 ) -> task_model.Task:
     original.title = task_create.title
     db.add(original)
-    db.commit()
-    db.refresh(original)
+    await db.commit()
+    await db.refresh(original)
     return original
 
 
-def create_task(db: Session, task_create: task_schema.TaskCreate) -> task_model.Task:
+async def create_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate
+) -> task_model.Task:
     task = task_model.Task(**task_create.dict())
     db.add(task)
-    db.commit()
-    db.refresh(task)
+    await db.commit()
+    await db.refresh(task)
     return task
 
 
--- api/routers/task.py
+++ api/routers/task.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from api.db import get_db
 from fastapi import APIRouter, Depends, HTTPException
+from sqlarchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
 import api.schemas.task as task_schema
@@ -10,13 +11,16 @@ router = APIRouter()
 
 
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    # return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+    return await task_crud.get_tasks_with_done(db)
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
-    return task_crud.create_task(db, task_body)
+async def create_task(
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    return await task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
